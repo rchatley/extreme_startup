3fadbcf8d427acbb054d36bc5fe6bcb0ea171484
Robert Chatley <robert@develogical.com>
2011-04-07 22:03:25 +0100

    catch exception when getting response e.g. connection refused

--- web_server.rb
+++ web_server.rb
@@ -63,16 +63,21 @@ class Shopper
       question = @question_factory.next_question
       url = @player.url + '?q=' + question.to_s.gsub(' ', '%20')
       puts "GET:" + url
-      response = HTTParty.get(url)
-      puts "question was " + question.to_s
-      puts "player #{@player.name} said #{response}"
-      if (question.answered_correctly(response)) then
-        puts "player #{@player.name} was correct"
-        @scoreboard.increment_score_for(@player)
-        sleep 5
-      else
-        puts "player #{@player.name} was wrong"
-        sleep 10
+      begin
+        response = HTTParty.get(url)
+        puts "question was " + question.to_s
+        puts "player #{@player.name} said #{response}"
+        if (question.answered_correctly(response)) then
+          puts "player #{@player.name} was correct"
+          @scoreboard.increment_score_for(@player)
+          sleep 5
+        else
+          puts "player #{@player.name} was wrong"
+          sleep 10
+        end
+      rescue
+        puts "player #{@player.name} was down - try again later"
+        sleep 20
       end
     end
   end
