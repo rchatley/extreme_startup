427beb5946a7344415e36a13a6cb3f508bbcb19f
Robert Chatley <robert@develogical.com>
2011-04-11 12:36:17 +0100

    move shopper to own file

---
+++ lib/extreme_startup/shopper.rb
@@ -0,0 +1,35 @@
+require_relative 'question_factory'
+
+module ExtremeStartup
+  class Shopper
+    def initialize(player, scoreboard)
+      @player = player
+      @scoreboard = scoreboard
+      @question_factory = QuestionFactory.new
+    end
+  
+    def start
+      while true
+        question = @question_factory.next_question
+        url = @player.url + '?q=' + question.to_s.gsub(' ', '%20')
+        puts "GET:" + url
+        begin
+          response = HTTParty.get(url)
+          puts "question was " + question.to_s
+          puts "player #{@player.name} said #{response}"
+          if (question.answered_correctly(response)) then
+            puts "player #{@player.name} was correct"
+            @scoreboard.increment_score_for(@player)
+            sleep 5
+          else
+            puts "player #{@player.name} was wrong"
+            sleep 10
+          end
+        rescue => exception
+          puts "player #{@player.name} was down - try again later #{exception}"
+          sleep 20
+        end
+      end
+    end
+  end
+end
--- web_server.rb
+++ web_server.rb
@@ -4,44 +4,12 @@ require 'httparty'
 require 'uuid'
 require 'haml'
 require_relative 'ip'
-require_relative 'lib/extreme_startup/question_factory'
 require_relative 'lib/extreme_startup/scoreboard'
 require_relative 'lib/extreme_startup/player'
+require_relative 'lib/extreme_startup/shopper'
 
 configure do
   set :port, 3000
-end
-
-class Shopper
-  def initialize(player, scoreboard)
-    @player = player
-    @scoreboard = scoreboard
-    @question_factory = ExtremeStartup::QuestionFactory.new
-  end
-  
-  def start
-    while true
-      question = @question_factory.next_question
-      url = @player.url + '?q=' + question.to_s.gsub(' ', '%20')
-      puts "GET:" + url
-      begin
-        response = HTTParty.get(url)
-        puts "question was " + question.to_s
-        puts "player #{@player.name} said #{response}"
-        if (question.answered_correctly(response)) then
-          puts "player #{@player.name} was correct"
-          @scoreboard.increment_score_for(@player)
-          sleep 5
-        else
-          puts "player #{@player.name} was wrong"
-          sleep 10
-        end
-      rescue => exception
-        puts "player #{@player.name} was down - try again later #{exception}"
-        sleep 20
-      end
-    end
-  end
 end
 
 $players = Hash.new
@@ -66,7 +34,7 @@ post '/players' do
   $scoreboard.new_player(player)
   $players[player.uuid] = player
 
-  Thread.new { Shopper.new(player, $scoreboard).start }
+  Thread.new { ExtremeStartup::Shopper.new(player, $scoreboard).start }
   
   personal_page = "http://#{local_ip}:#{@env["SERVER_PORT"]}/players/#{player.uuid}"  
   haml :player_added, :locals => { :url => personal_page }
