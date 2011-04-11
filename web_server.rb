afa353bb8fe44486b0c918f96601e09d1f6445f5
Robert Chatley <robert@develogical.com>
2011-04-11 13:03:36 +0100

    got rid of the global variables - converted to a more modular sinatra style

--- web_server.rb
+++ web_server.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 require 'httparty'
 require 'uuid'
 require 'haml'
@@ -8,34 +8,50 @@ require_relative 'lib/extreme_startup/scoreboard'
 require_relative 'lib/extreme_startup/player'
 require_relative 'lib/extreme_startup/shopper'
 
-configure do
-  set :port, 3000
-end
-
-$players = Hash.new
-$scoreboard = ExtremeStartup::Scoreboard.new
-
-get '/' do 
-  haml :leaderboard, :locals => { :leaderboard => $scoreboard.leaderboard, :players => $players  }
-end
-
-get %r{/players/([\w]+)} do |uuid|
-  haml :personal_page, :locals => { :name => $players[uuid].name, :score => $scoreboard.scores[uuid] }
-end
-
-get '/players' do
-  haml :add_player
-end
-
 Thread.abort_on_exception = true
 
-post '/players' do
-  player = ExtremeStartup::Player.new(params)
-  $scoreboard.new_player(player)
-  $players[player.uuid] = player
 
-  Thread.new { ExtremeStartup::Shopper.new(player, $scoreboard).start }
+
+module ExtremeStartup
+  class WebServer < Sinatra::Base
+
+    set :port, 3000
+    set :players,    Hash.new
+    set :scoreboard, Scoreboard.new
+    
+    def players
+      settings.players
+    end
+
+    def scoreboard
+      settings.scoreboard
+    end
+
+    get '/' do 
+      haml :leaderboard, :locals => { 
+          :leaderboard => scoreboard.leaderboard, 
+          :players => players  }
+    end
+
+    get %r{/players/([\w]+)} do |uuid|
+      haml :personal_page, :locals => { :name => players[uuid].name, :score => scoreboard.scores[uuid] }
+    end
+
+    get '/players' do
+      haml :add_player
+    end
+
+    post '/players' do
+      player = Player.new(params)
+      scoreboard.new_player(player)
+      players[player.uuid] = player
+
+      Thread.new { Shopper.new(player, scoreboard).start }
   
-  personal_page = "http://#{local_ip}:#{@env["SERVER_PORT"]}/players/#{player.uuid}"  
-  haml :player_added, :locals => { :url => personal_page }
+      personal_page = "http://#{local_ip}:#{@env["SERVER_PORT"]}/players/#{player.uuid}"
+      haml :player_added, :locals => { :url => personal_page }
+    end
+  end
 end
+
+ExtremeStartup::WebServer.run!
