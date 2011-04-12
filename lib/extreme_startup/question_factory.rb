ab12e6f607668ebef9ac183cf9606efe7f8bb772
Robert Chatley <robert@develogical.com>
2011-04-12 10:30:21 +0100

    add a Fibonacci question

--- lib/extreme_startup/question_factory.rb
+++ lib/extreme_startup/question_factory.rb
@@ -13,6 +13,20 @@ module ExtremeStartup
     end
   end
   
+  class UnaryMathsQuestion
+    def initialize(*numbers)
+      if numbers.any?
+        @n = *numbers.first
+      else
+        @n = rand(5)
+      end
+    end
+      
+    def answered_correctly?(answer) 
+      correct_answer.to_s.strip == answer.to_s.strip
+    end
+  end
+  
   class AdditionQuestion < BinaryMathsQuestion
     def to_s
       "what is #{@n1} plus #{@n2}"
@@ -50,6 +64,20 @@ module ExtremeStartup
   private
     def correct_answer
       @n1 ** @n2
+    end
+  end
+  
+  class FibonacciQuestion < BinaryMathsQuestion 
+    def to_s
+      n = @n1 + 4
+      "what is the #{n}th number in the Fibonacci sequence"
+    end
+  private
+    def correct_answer
+      n = @n1 + 4
+      root5 = Math.sqrt(5)
+      phi = 0.5 + root5/2
+      Integer(0.5 + phi**n/root5)
     end
   end
   
