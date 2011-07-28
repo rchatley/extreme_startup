c978d37a9385f07d54c34c0cfe17055658f52c6a
Robert Chatley <robert@develogical.com>
2011-07-28 21:26:00 +0100

    put the javascript in a separate file

---
+++ public/js/leaderboard.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	var colourTable = {}
+
+	var Graph = function(canvas) {
+		var timeSeries = {};
+		var smoothie = new SmoothieChart({millisPerPixel: 500});
+		smoothie.streamTo(canvas, 1000); 
+		var randomRgbValue = function () {
+			return Math.floor(Math.random() * 156 + 99);
+		}
+		var randomColour = function () {
+			return 'rgb(' + [randomRgbValue(), randomRgbValue(), randomRgbValue()].join(',') + ')';
+		}
+		this.updateWith = function (leaderboard) {
+			for (var i=0; i < leaderboard.length; i += 1) {
+				var entry = leaderboard[i];
+				var series = timeSeries[entry.playerid];
+				if (!series) {
+					series = timeSeries[entry.playerid] = new TimeSeries();
+					colourTable[entry.playerid] = randomColour();
+					smoothie.addTimeSeries(series, { strokeStyle:colourTable[entry.playerid], lineWidth:3 });
+				}
+				series.append(new Date().getTime(), entry.score);
+			}
+		};
+	};  
+
+	var ScoreBoard = function(div) {
+		this.updateWith = function (leaderboard) {
+			var list = $('<ul id="scoreboard"></ul>');            
+			for (var i=0; i < leaderboard.length; i += 1) {
+				var entry = leaderboard[i];
+				list.append(
+					$('<div/>').append(
+						$('<li/>', {class: "player"})
+						.append($('<div>' + entry.playername + '</div>').addClass("ranking name").css("background-color", colourTable[entry.playerid]))
+						.append($('<div>' + entry.score + '</div>').addClass("ranking points").css("background-color", colourTable[entry.playerid]))
+						.append($('<a>Withdraw</a>').attr("href", "/withdraw/" + entry.playerid))));
+				}
+				$("#scoreboard").replaceWith(list); 
+			}
+		};
+
+		var graph = new Graph($('#mycanvas')[0]);  // get DOM object from jQuery object
+		var scoreboard = new ScoreBoard($('#scoreboard'));
+
+		setInterval(function() {
+			$.ajax({
+				url: '/scores',
+				success: function( data ) {
+					var leaderboard = JSON.parse(data);
+					graph.updateWith(leaderboard);
+					scoreboard.updateWith(leaderboard);
+				}
+			});
+		}, 1000);
+	}
+);
