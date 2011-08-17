75331e06e887a4eb0d46e37187ff73f9f434bf11
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2011-08-17 15:12:41 +0200

    store correct answer to GeneralKnowledgeQuestion in property correct_answer.
    correct answer was previously stored in @answer, whose value will be overwritten with response.to_s in Question::ask.
    So every answer to a GeneralKnowledgeQuestion would be correct.

--- lib/extreme_startup/question_factory.rb
+++ lib/extreme_startup/question_factory.rb
@@ -313,7 +313,7 @@ module ExtremeStartup
     def initialize(player)
       question = GeneralKnowledgeQuestion.question_bank.sample
       @question = question[0]
-      @answer = question[1]
+      @correct_answer = question[1]
     end
     
     def as_text
@@ -321,7 +321,7 @@ module ExtremeStartup
     end
     
     def correct_answer
-      @answer
+      @correct_answer
     end
   end
   
