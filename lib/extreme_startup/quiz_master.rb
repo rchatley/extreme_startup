32a46b81796bc5a085dc684db00ce6b968dd903e
Robert Chatley <robert@develogical.com>
2011-11-26 15:49:48 +0000

    give each QuizMaster a RateController

--- lib/extreme_startup/quiz_master.rb
+++ lib/extreme_startup/quiz_master.rb
@@ -3,11 +3,18 @@ require 'uri'
 
 module ExtremeStartup
 
+  class RateController
+    def wait_for_next_request(question)
+      sleep question.delay_before_next
+    end
+  end
+
   class QuizMaster
     def initialize(player, scoreboard, question_factory)
       @player = player
       @scoreboard = scoreboard
       @question_factory = question_factory
+      @rate_controller = RateController.new
     end
 
     def player_passed?(response)
@@ -20,7 +27,7 @@ module ExtremeStartup
         question.ask(@player)
         puts "For player #{@player}\n#{question.display_result}"
         @scoreboard.increment_score_for(@player, question)
-        sleep question.delay_before_next
+        @rate_controller.wait_for_next_request(question)
       end
     end
 
