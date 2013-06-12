40ebb5b244f62d42ebe39c04e3968abced8efb47
Arnaud Bailly <abailly@murex.com>
2013-06-12 10:47:38 +0200

    replace smoothie with flot for graphing on /graph page

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,66 +1,77 @@
-$(document).ready(function() {
-	var colourTable = {}
+$(document).ready(function () {
+        var MAX_LENGTH = 100;
+        var colourTable = {}
 
-	var Graph = function(canvas) {
-		var timeSeries = {};
-		var smoothie = new SmoothieChart({millisPerPixel: 500});
-		smoothie.streamTo(canvas, 1000); 
-		var randomRgbValue = function () {
-			return Math.floor(Math.random() * 156 + 99);
-		}
-		var randomColour = function () {
-			return 'rgb(' + [randomRgbValue(), randomRgbValue(), randomRgbValue()].join(',') + ')';
-		}
-		this.updateWith = function (leaderboard) {
-			for (var i=0; i < leaderboard.length; i += 1) {
-				var entry = leaderboard[i];
-				var series = timeSeries[entry.playerid];
-				if (!series) {
-					series = timeSeries[entry.playerid] = new TimeSeries();
-					colourTable[entry.playerid] = randomColour();
-					smoothie.addTimeSeries(series, { strokeStyle:colourTable[entry.playerid], lineWidth:3 });
-				}
-				series.append(new Date().getTime(), entry.score);
-				smoothie.start();
-			}
-		};
-		this.pause = function() {
-			smoothie.stop();
-		}
-	};  
+        var Graph = function () {
+            var timeSeries = {};
+            var randomRgbValue = function () {
+                return Math.floor(Math.random() * 156 + 99);
+            }
+            var randomColour = function () {
+                return 'rgb(' + [randomRgbValue(), randomRgbValue(), randomRgbValue()].join(',') + ')';
+            }
+            var getSeries = function (pid) {
+                return timeSeries[pid];
+            }
+            this.updateWith = function (leaderboard) {
+                for (var i = 0; i < leaderboard.length; i += 1) {
+                    var entry = leaderboard[i];
+                    var series = timeSeries[entry.playerid];
+                    if (!series) {
+                        colourTable[entry.playerid] = randomColour();
+                        series = timeSeries[entry.playerid] = {
+                            label: entry.playername,
+                            data: [],
+                            lines: { lineWidth: 0.8},
+                            color: colourTable[entry.playerid]};
+                    }
+                    if(series.data.length > MAX_LENGTH) {
+                        series.data.shift();
+                    }
+                    series.data.push([new Date().getTime(), entry.score]);
+                }
+                var elems = [];
+                for (var p in timeSeries) {
+                    if (timeSeries.hasOwnProperty) {
+                        elems.push(timeSeries[p]);
+                    }
+                }
+                $.plot($('#mycanvas'), elems,
+                    {xaxis: {mode: "time"},
+                    legend: {show: false}});
+            };
+        };
 
-	var ScoreBoard = function(div) {
-		this.updateWith = function (leaderboard) {
-			var list = $('<ul id="scoreboard"></ul>');            
-			for (var i=0; i < leaderboard.length; i += 1) {
-				var entry = leaderboard[i];
-				list.append(
-					$('<div/>').append(
-						$('<li/>', {class: "player"})
-						.append($('<div>' + entry.playername + '</div>').addClass("ranking name").css("background-color", colourTable[entry.playerid]))
-						.append($('<div>' + entry.score + '</div>').addClass("ranking points").css("background-color", colourTable[entry.playerid]))
-						.append($('<a>Withdraw</a>').attr("href", "/withdraw/" + entry.playerid))));
-				}
-				$("#scoreboard").replaceWith(list); 
-			}
-		};
+        var ScoreBoard = function (div) {
+            this.updateWith = function (leaderboard) {
+                var list = $('<ul id="scoreboard"></ul>');
+                for (var i = 0; i < leaderboard.length; i += 1) {
+                    var entry = leaderboard[i];
+                    list.append(
+                        $('<div/>').append(
+                            $('<li/>', {class: "player"})
+                                .append($('<div>' + entry.playername + '</div>').addClass("ranking name").css("background-color", colourTable[entry.playerid]))
+                                .append($('<div>' + entry.score + '</div>').addClass("ranking points").css("background-color", colourTable[entry.playerid]))
+                                .append($('<a>Withdraw</a>').attr("href", "/withdraw/" + entry.playerid))));
+                }
+                $("#scoreboard").replaceWith(list);
+            }
+        };
 
-		var graph = new Graph($('#mycanvas')[0]);  // get DOM object from jQuery object
-		var scoreboard = new ScoreBoard($('#scoreboard'));
+        var graph = new Graph();  // get DOM object from jQuery object
+        var scoreboard = new ScoreBoard($('#scoreboard'));
 
-		setInterval(function() {
-			$.ajax({
-				url: '/scores',
-				success: function( data ) {
-					var leaderboard = JSON.parse(data);
-					if (leaderboard.inplay) {
-						graph.updateWith(leaderboard.entries);
-						scoreboard.updateWith(leaderboard.entries);
-					} else {
-						graph.pause();
-					}
-				}
-			});
-		}, 1000);
-	}
+        setInterval(function () {
+            $.ajax({
+                url: '/scores',
+                success: function (data) {
+                    var leaderboard = JSON.parse(data);
+                    if (leaderboard.inplay) {
+                        graph.updateWith(leaderboard.entries);
+                        scoreboard.updateWith(leaderboard.entries);
+                    }
+                }
+            });
+        }, 10000);
+    }
 );
